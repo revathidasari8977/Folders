670ee098e081d7c11220946e272efc10fa037fe1
revathidasari8977 <135516311+revathidasari8977@users.noreply.github.com>
2023-06-11 19:41:16 +0530

    Add files via upload

---
+++ MyHibernate/src/main/java/snippet/Snippet.java
@@ -0,0 +1,14 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		<!-- https://mvnrepository.com/artifact/org.hibernate/hibernate-core -->
+		<dependency>
+		    <groupId>org.hibernate</groupId>
+		    <artifactId>hibernate-core</artifactId>
+		    <version>5.6.10.Final</version>
+		</dependency>
+		
+	}
+}
+
